c03f1e116b2bf8c4d3f365a4b1d2c7265b5a8348
James Chase <gochases@gmail.com>
2024-02-14 11:15:31 -0700

    Add informative comments

--- hello_world/Main.java
+++ hello_world/Main.java
@@ -1,5 +1,17 @@
+// Name: James A. Chase
+// File: Main.java
+// Date: 14 February 2024
+
+// Name of file MUST match class name
+// A class should always start with an uppercase letter
+// Java is case-sensitive
 public class Main {
     public static void main(String[] args) {
+        // Strings must be wrapped in double quotes
+        // The 'print' method also works, but doesn't leave a newline at the end
         System.out.println("Hello world!");
     }
 }
+
+// To compile: javac Main.java
+// To run: java Main
