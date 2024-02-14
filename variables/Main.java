40ffddce3a0cf02313dd89f64b1ac6061b27a56b
James Chase <gochases@gmail.com>
2024-02-14 11:17:44 -0700

    Add variables project

---
+++ variables/Main.java
@@ -0,0 +1,11 @@
+// Name: James A. Chase
+// File: Main.java
+// Date: 14 February 2024
+
+package variables;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
