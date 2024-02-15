34755e6fe4c5ffa28b8bfe74813a4b8174442f4c
James Chase <gochases@gmail.com>
2024-02-14 18:51:24 -0700

    Add variables code

--- variables/Main.java
+++ variables/Main.java
@@ -2,10 +2,18 @@
 // File: Main.java
 // Date: 14 February 2024
 
-package variables;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        String name = "James";
+        int num = 42;
+        // to create a constant in Java, we use the 'final' keyword
+        final int newNum = 84;
+        float myFloat = 5.5f;
+        boolean bool = true;
+        System.out.println(name);
+        System.out.println(num);
+        System.out.println(newNum);
+        System.out.println(myFloat);
+        System.out.println(bool);
     }
 }
