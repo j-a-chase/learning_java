85b3d91ef63b8ef31bbee0597ffdcd019f8b0d4e
James Chase <gochases@gmail.com>
2024-02-15 11:31:46 -0700

    Add data types notes

---
+++ data_types/Main.java
@@ -0,0 +1,47 @@
+// Name: James A. Chase
+// File: Main.java
+// Date: 14 February 2024
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Primitive Data Types:
+         *  - byte (1 byte)
+         *      - -128 -> 127
+         *      - saves memory over int when you know value will be in that range
+         *  - short (2 bytes)
+         *      - -32768 -> 23767
+         *  - int (4 bytes)
+         *      - -2147483648 -> 2147483647
+         *  - long (8 bytes)
+         *      - -9223372036854775808 -> 9223372036854775807
+         *      - if int isn't big enough
+         *      - ends in L
+         *  - float (4 bytes)
+         *      - precise to 6 or 7 digits
+         *      - can be specified to be a scientific number with e to power of 10
+         *  - double (8 bytes)
+         *      - precise to about 15 digits
+         *      - preferred for most calculations
+         *      - can be specified to be a scientific number with E to power of 10
+         *  - boolean (1 bit)
+         *      - 0 or 1, false or true, etc.
+         *  - char (2 bytes)
+         *      - stores a single character in SINGLE quotes
+         *      - can also be assigned with ASCII values instead
+         *  - Always have a value
+         *  - Starts with lowercase
+         * 
+         * Non-primitive Data Types:
+         *  - String
+         *      - Stores a sequence of characters (text)
+         *      - double quotes!
+         *      - Used often and is integrated so much in Java that some refer to it as "the special ninth type"
+         *  - Arrays
+         *  - Classes
+         *  - Can be null
+         *  - Starts with uppercase
+         */
+        System.out.println("Notes in comments!");
+    }
+}
