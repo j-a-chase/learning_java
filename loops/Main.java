9589d45b6ffbc0c8cbbe30b0d9544a50eb1677b7
James Chase <gochases@gmail.com>
2024-02-15 12:26:42 -0700

    Add loops notes

---
+++ loops/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is really just notes on for-each loops since I'm comfortable with everything else
+        String[] cars = {"Ford", "Toyota", "Nissan"};
+
+        for (String car : cars) {
+            System.out.println(car);
+        }
+    }
+}
