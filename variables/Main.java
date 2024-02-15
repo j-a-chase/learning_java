681f2fe92e479698e8a1b0576d94f657dd96cfa3
James Chase <gochases@gmail.com>
2024-02-14 19:04:17 -0700

    Add extra variables notes

--- variables/Main.java
+++ variables/Main.java
@@ -6,14 +6,28 @@ public class Main {
     public static void main(String[] args) {
         String name = "James";
         int num = 42;
+
         // to create a constant in Java, we use the 'final' keyword
         final int newNum = 84;
         float myFloat = 5.5f;
         boolean bool = true;
+
         System.out.println(name);
         System.out.println(num);
         System.out.println(newNum);
         System.out.println(myFloat);
         System.out.println(bool);
+        
+        // we can also concatenate in our println function!
+        System.out.println("Hello " + name);
+
+        // if we want to assign multiple values of the same type, we can do so using a comma-separated list
+        int x = 3, y = 4, z = 5;
+        System.out.println(x + y + z);
+
+        // we can also assign one value to multiple variables at once
+        int a, b, c;
+        a = b = c = 5;
+        System.out.println(a + b + c);
     }
 }
