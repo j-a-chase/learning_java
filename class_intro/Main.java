5843d14f8cf296dc91ed2e04db87b810d7fea5ad
James Chase <gochases@gmail.com>
2024-02-15 12:58:09 -0700

    Add some notes on classes

---
+++ class_intro/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int x = 5;
+    
+    // Here we create an instance of the Main object and print the value of its attribute, x
+    public static void main(String[] args) {
+        Main obj = new Main();
+        System.out.println(obj.x);
+    }
+}
