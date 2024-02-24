0d580391337c5a02faaa6ea587603a104b2aec65
James Chase <gochases@gmail.com>
2024-02-24 12:36:05 -0700

    Add regex notes with example

--- other_useful_packages/RegexExample.java
+++ other_useful_packages/RegexExample.java
@@ -7,7 +7,15 @@
  * This package includes:
  *  - Pattern (class) -> defines a pattern (to be used in a search)
  *  - Matcher (class) -> used to search for the pattern
- *  - PatternSyntaxException (class) -> Indicates syntax error in a regex pattern
+ *  - PatternSyntaxException (class) -> Indicates syntax error in a regex 
+ *      pattern
+ * 
+ * Flags in the 'compile' method:
+ *  - Pattern.CASE_INSENSITIVE -> case of letters ignored when performing search
+ *  - Pattern.LITERAL -> special characters in the pattern will not have any
+ *      special meaning and treated as ordinary characters
+ *  - Pattern.UNICODE_CASE -> use with CASE_INSENSITIVE to also ignore letters
+ *      outside of the English alphabet
 */
 
 import java.util.regex.Matcher;
@@ -15,9 +23,13 @@ import java.util.regex.Pattern;
 
 public class RegexExample {
     public static void main(String[] args) {
+        // This creates the pattern to search for using the compile method
         Pattern pattern = Pattern.compile("ur mom", Pattern.CASE_INSENSITIVE);
+
+        // This returns a matcher object which contains information about the search
         Matcher matcher = pattern.matcher("I can't believe you told me a 'ur mom' joke!");
 
+        // returns true if pattern was found, false otherwise
         if (matcher.find()) {
             System.out.println("Match found!");
         } else {
